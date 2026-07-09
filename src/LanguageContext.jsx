c3cd785dc51ede0e672d07296be40c39b5d12828
BrandonDRoger <acnologiaft9@gmail.com>
2026-07-09 18:26:39 -0400

    anglais par defaut

--- src/LanguageContext.jsx
+++ src/LanguageContext.jsx
@@ -4,7 +4,7 @@ import { translations } from './translations'
 const LanguageContext = createContext(null)
 
 export function LanguageProvider({ children }) {
-  const [lang, setLang] = useState('fr')
+  const [lang, setLang] = useState('en')
 
   useEffect(() => {
     document.documentElement.lang = lang
